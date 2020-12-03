4bb668f2d16b6b3465b9f0156e2a0e7867e7ec91
daniel_feleppa <dfeleppa@gmail.com>
2020-12-03 08:14:17 -0500

    add omniauth to athlete

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -1,6 +1,15 @@
 class Athlete < ApplicationRecord
     belongs_to :coach
-    has_many :sessions
-    has_many :workouts, through: :sessions
+    has_many :workouts
+    has_many :movements, through: :workouts
     has_secure_password
+    validates :email, presence: true
+    validates :email, uniqueness: true
+
+    def self.from_omniauth(auth)
+          where(email: auth.info.email).first_or_initialize do |athlete|
+            user.email = auth.info.email
+            user.password = SecureRandom.hex
+        end
+    end
 end
